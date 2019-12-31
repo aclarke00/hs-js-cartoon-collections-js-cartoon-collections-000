3f8a7ddef67d81eaf6217c8a510a1b85a164bfaa
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-12-31 04:45:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,13 +42,29 @@ function findTheCheese (foods) {
   
   let cheese = ["cheddar", "gouda", "camembert"];
   
-  for (let i=0; i < cheese.length; i++) {
-    for (let j=0; j < foods.length; j++) {
-      if (foods[j] === cheese[i]) {
-        return cheese[i];
-      } 
-    }
+  for (let i=0; i < foods.length; i++) {
+    // for (let j=0; j < cheese.length; j++) {
+    //   if (foods[j] === cheese[i]) {
+      cheese.indexOf(foods[i])
+        let cheeseIndex = cheese.indexOf(foods[i])
+        if (cheeseIndex > -1) {
+          return foods[i]
+        }
+    //   } 
+    // }
   }
   
   return "no cheese!"
 }
+
+// return a new array with all words from first array that start with B 
+function wordsThatStartWithB (words) {
+  let myArr = [];
+  for (let i=0; i < words.length; i++) {
+    if (words[i].startsWith('B')){
+      myArr.push(words[i])
+    }
+  }
+  return myArr
+}
+
